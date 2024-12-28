074d059439f3e04ae0413bedacaeb1b22cf232da
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-12-28 23:04:10 +0100

    List messages and enable creation of new ones

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const indexRouter = require('./routers/indexRouter');
+const path = require('node:path');
 
 const app = express();
 const PORT = 3000;
 
-app.use('/', (req, res) => res.send('Homepage'));
-app.use('/new', (req, res) => res.send('New message'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+app.use('/new', (req, res) => res.render('form'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.listen(PORT, () =>
   console.log(`Server running - listening on port ${PORT}`)
---
+++ routers/indexRouter.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+
+const indexRouter = Router();
+
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+indexRouter.get('/', (req, res) => res.render('index', { messages }));
+indexRouter.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect('/');
+});
+
+module.exports = indexRouter;
