8d61a4d24fc3c82faaa512b48718df0e19849dad
mateoo04 <144594500+mateoo04@users.noreply.github.com>
2024-12-29 09:37:26 +0100

    Create message details interface

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -4,11 +4,13 @@ const indexRouter = Router();
 
 const messages = [
   {
+    id: 1,
     text: 'Hi there!',
     user: 'Amando',
     added: new Date(),
   },
   {
+    id: 2,
     text: 'Hello World!',
     user: 'Charles',
     added: new Date(),
@@ -16,8 +18,21 @@ const messages = [
 ];
 
 indexRouter.get('/', (req, res) => res.render('index', { messages }));
+indexRouter.get('/message', (req, res) => {
+  const message = messages.find(
+    (message) => message.id === Number(req.query.id)
+  );
+  res.render('messageDetails', {
+    id: message.id,
+    user: message.user,
+    text: message.text,
+    added: message.added,
+  });
+});
+
 indexRouter.post('/new', (req, res) => {
   messages.push({
+    id: Math.max(...messages.map((message) => message.id)) + 1,
     text: req.body.messageText,
     user: req.body.user,
     added: new Date(),
